b2c8e1c641a0856aa5cea7fec8feca77eefdd357
chu-andrew <chu.andrew.h@gmail.com>
2024-07-27 17:23:56 -0700

    rm default styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import '@mantine/core/styles.css';
 
-const inter = Inter({ subsets: ["latin"] });
+import { MantineProvider } from '@mantine/core';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
