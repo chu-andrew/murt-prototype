bf3086f337a02bfae31cf04b2dbb50c30ed66f5a
chu-andrew <chu.andrew.h@gmail.com>
2024-08-13 00:21:10 -0700

    format

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -1,16 +1,16 @@
-import {createTheme, MantineColorsTuple, MantineTheme} from '@mantine/core';
+import { createTheme, MantineColorsTuple, MantineTheme } from "@mantine/core";
 
 const violet: MantineColorsTuple = [
-  '#f6ecff',
-  '#e7d6fb',
-  '#caabf1',
-  '#ac7ce8',
-  '#9354e0',
-  '#833cdb',
-  '#7b2eda',
-  '#6921c2',
-  '#5d1cae',
-  '#501599'
+  "#f6ecff",
+  "#e7d6fb",
+  "#caabf1",
+  "#ac7ce8",
+  "#9354e0",
+  "#833cdb",
+  "#7b2eda",
+  "#6921c2",
+  "#5d1cae",
+  "#501599",
 ];
 
 export const McMurtryTheme = createTheme({
