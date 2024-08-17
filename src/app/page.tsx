4715e966b12843fe96bf999f6f9842966e4a5087
chu-andrew <chu.andrew.h@gmail.com>
2024-08-16 22:16:58 -0700

    home page setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { Text } from "@mantine/core";
+import cx from "clsx";
+import { Title, Text, Container, Button, Overlay } from "@mantine/core";
+import classes from "./page.module.css";
 
-export default function Home() {
+export default function Homepage() {
   return (
-    <main>
-      <div>
-        <Text>McMurtry College</Text>
+    <div className={classes.wrapper}>
+      <Overlay color="#000" opacity={0.65} zIndex={1} />
+
+      <div className={classes.inner}>
+        <Title className={classes.title}>Welcome to McMurtry College!</Title>
+
+        <Container size={640}>
+          <Text size="lg" className={classes.description}>
+            The tenth and greatest residential college at Rice University
+          </Text>
+        </Container>
       </div>
-    </main>
+    </div>
   );
 }
