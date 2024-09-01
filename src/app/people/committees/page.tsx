a8aecaa8d30d55e8d6b1980a2177d3d66396ecb5
chu-andrew <chu.andrew.h@gmail.com>
2024-09-01 12:30:19 -0500

    committees template

---
+++ src/app/people/committees/page.tsx
@@ -0,0 +1,10 @@
+import PageLayout from "@/components/page-layout/PageLayout";
+import { title, description } from "./data";
+
+export default function Committees() {
+  return (
+    <PageLayout title={title} description={description}>
+      Hello
+    </PageLayout>
+  );
+}
